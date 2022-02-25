36c42fb577d75845b6a19c80e879353bdc1b9100
soma <soma@velosimo.com>
2022-02-25 12:58:59 -0500

    Using LoadableComponents instead of React.lazy

--- src/containers/application/Application.js
+++ src/containers/application/Application.js
@@ -2,10 +2,11 @@ import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../../components/layout/MainLayout";
 import Loading from "../../components/loading/Loading";
+import { lazy } from "@loadable/component";
 
-const HomeAsync = React.lazy(() => import('../home/Home'));
-const NotFoundAsync = React.lazy(() => import('../notFound/NotFound'));
-const ExampleAsync = React.lazy(() => import('../example/Example'));
+const HomeAsync = lazy(() => import('../home/Home'));
+const NotFoundAsync = lazy(() => import('../notFound/NotFound'));
+const ExampleAsync = lazy(() => import('../example/Example'));
 
 const Application = () => {
   return (
