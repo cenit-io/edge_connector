cdb6ae797646dd342a958fb2e83a4a52b678ce2f
Hansel Bormey <hanselbormey@gmail.com>
2022-04-08 23:38:26 -0400

    Add submit create integration

--- src/containers/channels/AddIntegration.js
+++ src/containers/channels/AddIntegration.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+
 import { FormattedMessage, useIntl } from 'react-intl';
 
-import Transition from '../../components/common/global/Transition';
+import Dialog from '../../components/common/Dialog';
 
-const AddChannel = ({ data, handleAccept, handleClose }) => {
+const AddIntegration = ({ data, loadingOnSubmit, handleAccept, handleClose }) => {
   const intl = useIntl();
   const [name, setName] = useState('');
   const [authorized, setAuthorized] = useState(false);
@@ -28,21 +24,12 @@ const AddChannel = ({ data, handleAccept, handleClose }) => {
   };
 
   const beforeHandleAccept = () => {
-    handleAccept({ ...data, name, authorized });
+    handleAccept({ name, authorized });
   }
 
   return (
-    <Dialog
-      open
-      TransitionComponent={Transition}
-      keepMounted
-      scroll="paper"
-      onClose={handleClose}
-      aria-describedby="alert-dialog-slide-description"
-    >
-      <DialogTitle><FormattedMessage id="add.integration" /></DialogTitle>
-      <DialogContent dividers sx={{ width: '450px' }}>
-        <Box sx={{ display: 'flex', mb: 2 }}>
+    <Dialog title={<FormattedMessage id="add.integration" />} open onAccept={beforeHandleAccept} onClose={handleClose} showLoadingOnSubmit={loadingOnSubmit} disabledAcceptBtn={!name?.trim().length}>
+      <Box sx={{ display: 'flex', mb: 2 }}>
           <Typography sx={{ mr: 1, fontWeight: 'bold' }} color="primary">
             <FormattedMessage id="channel" />:
           </Typography>
@@ -65,20 +52,11 @@ const AddChannel = ({ data, handleAccept, handleClose }) => {
           control={<Checkbox checked={authorized} onChange={handleAuthorized} />}
           label={intl.formatMessage({ id: 'authorized' })}
         />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} variant="outlined">
-          <FormattedMessage id="actions.cancel" />
-        </Button>
-        <Button onClick={beforeHandleAccept} variant="contained" disabled={!name.trim().length}>
-          <FormattedMessage id="actions.save" />
-        </Button>
-      </DialogActions>
     </Dialog>
   );
 };
 
-AddChannel.propTypes = {
+AddIntegration.propTypes = {
   handleAccept: PropTypes.func.isRequired,
   handleClose: PropTypes.func.isRequired,
   data: PropTypes.shape({
@@ -89,4 +67,4 @@ AddChannel.propTypes = {
   }).isRequired
 };
 
-export default AddChannel;
+export default AddIntegration;
