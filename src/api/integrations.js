562f5e97756fbaa1d42fb42f90a7693a9cc2d740
Hansel Bormey <hanselbormey@gmail.com>
2022-04-08 23:29:42 -0400

    Add 'create integration' and 'authorize' services

--- src/api/integrations.js
+++ src/api/integrations.js
@@ -1,4 +1,4 @@
-import API from '../utils/api';
+import API, { getHeaders } from '../utils/api';
 
 export const getConnectedIntegrations = async () => {
     const params = {
@@ -50,3 +50,27 @@ export const getChannels = async (payload = {}) => {
         throw new Error(error.response?.data?.message || 'Error getting channels')
     }
 };
+
+export const createIntegration = async (params) => {
+    const { channel, name } = params;
+    try {
+        const response = await API.post('/integrations', {
+            data: { name, channel }
+        });
+        return response.data;
+
+    } catch (error) {
+        throw new Error(error.response?.data?.message || 'Error creating integration')
+    }
+};
+
+export const authorize = (id) => {
+    const path = `integrations/${id}/authorize`;
+    const redirectUri = `${process.env.REACT_APP_URL}/integrations/connected-integrations`;
+    window.open(
+        `${process.env.REACT_APP_ECAPI_URL}/${path}?redirect_uri=${redirectUri}&${getHeaders(
+            path, `${process.env.REACT_APP_URL}/integrations/connected-integrations`
+        )}`
+    );
+};
+
