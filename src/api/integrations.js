1236c861414709337602173ede2323c6f6ee7ca5
Hansel Bormey <hanselbormey@gmail.com>
2022-04-12 17:09:46 -0400

    Add unauthorize service

--- src/api/integrations.js
+++ src/api/integrations.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import API, { getHeaders } from '../utils/api';
 
 export const getConnectedIntegrations = async () => {
@@ -52,25 +53,37 @@ export const getChannels = async (payload = {}) => {
 };
 
 export const createIntegration = async (params) => {
-    const { channel, name } = params;
-    try {
-        const response = await API.post('/integrations', {
-            data: { name, channel }
-        });
-        return response.data;
+  const { channel, name } = params;
+  try {
+    const response = await API.post('/integrations', {
+      data: { name, channel }
+    });
+    return response.data;
 
-    } catch (error) {
-        throw new Error(error.response?.data?.message || 'Error creating integration')
-    }
+  } catch (error) {
+    throw new Error(error.response?.data?.message || 'Error creating integration')
+  }
 };
 
 export const authorize = (id) => {
-    const path = `integrations/${id}/authorize`;
-    const redirectUri = `${process.env.REACT_APP_URL}/integrations/connected-integrations`;
-    window.open(
-        `${process.env.REACT_APP_ECAPI_URL}/${path}?redirect_uri=${redirectUri}&${getHeaders(
-            path, `${process.env.REACT_APP_URL}/integrations/connected-integrations`
-        )}`
-    );
+  const path = `integrations/${id}/authorize`;
+  const redirectUri = `${process.env.REACT_APP_URL}/integrations/connected-integrations`;
+  window.open(
+    `${process.env.REACT_APP_ECAPI_URL}/${path}?redirect_uri=${redirectUri}&${getHeaders(
+      path, `${process.env.REACT_APP_URL}/integrations/connected-integrations`
+    )}`
+  );
 };
+
+export const unauthorize = async (id) => {
+  try {
+    const response = await API.delete(`integrations/${id}/authorize`, {
+      data: { data: { integration_id: id } }
+    });
+    return response.data;
+
+  } catch (error) {
+    throw new Error(error.response?.data?.message || 'Error unauthorizing integration')
+  }
+}
 
